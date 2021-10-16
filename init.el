e09d047676161946e30548a8828bf2deb5f287c6
Ye Peng <yemouren@proonmail.com>
2021-10-16 22:22:01 +0800

    I don't want write this

--- init.el
+++ init.el
@@ -23,8 +23,8 @@
        (company +childframe)             ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +prescient +icons)   ; a search engine for love and life
-       ;;(vertico +icons)            ; the search engine of the future
+       ;; (ivy +prescient +icons)   ; a search engine for love and life
+       (vertico +icons)            ; the search engine of the future
 
        :ui
        deft                             ; notational velocity for Emacs
@@ -36,17 +36,17 @@
        hl-todo                          ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides                    ; highlighted indent columns
-       (ligatures +extra) ;ligatures and symbols to make your code pretty again
+       ;; (ligatures +fira) ;ligatures and symbols to make your code pretty again
        minimap                          ; show a map of the code on the side
        modeline                         ; snazzy, Atom-inspired modeline, plus API
        nav-flash                        ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints                          ; highlight the region an operation acts on
        (popup +defaults)                ; tame sudden yet inevitable temporary windows
-       tabs                     ; a tab bar for Emacs
+       ;;tabs                     ; a tab bar for Emacs
        treemacs                         ; a project drawer, like neotree but cooler
-       unicode                          ; extended unicode support for various languages
-       vc-gutter                        ; vcs diff in the fringe
+       ;;unicode                          ; extended unicode support for various languages
+       vc-gutter
        vi-tilde-fringe                  ; fringe tildes to mark beyond EOB
        (window-select +numbers)                     ; visually switch windows
        workspaces                       ; tab emulation, persistence & separate workspaces
@@ -82,13 +82,13 @@
        :checkers
        (syntax +childframe)                            ; tasing you for every semicolon you forget
        ;; (spell +hunspell)    ; tasing you for misspelling mispelling
-       grammar                          ; tasing grammar mistake every you make
+       ;;grammar                          ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        debugger                         ; FIXME stepping through code, to help you add bugs
-       direnv
-       docker
+       ;; direnv
+       ;; docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)                  ; run code, run (also, repls)
@@ -114,13 +114,13 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       (cc +lsp)                        ; C/C++/Obj-C madness
+       cc                         ; C/C++/Obj-C madness
        ;;(clojure +lsp)    ; java with a lisp
        ;;common-lisp                      ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       data              ; config/data formats
+       ;; data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
@@ -131,21 +131,21 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)         ; the hipster dialect
+       ;; (go +lsp)         ; the hipster dialect
        ;;(haskell +dante)                 ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;(json +lsp)                      ; At least it ain't XML
        (java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;(javascript +lsp)                ; all(hope(abandon(ye(who(enter(here))))))
+       ;; (javascript +lsp)                ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;(latex +latexmk +cdlatex +lsp +fold)          ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger         ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
-       (markdown +grip)                 ; writing docs for people to ignore
+       ;; lua               ; one-based indices? one-based indices
+       ;; (markdown +grip)                 ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
@@ -167,13 +167,13 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +racket)               ; a fully conniving family of lisps
+       ;; (scheme +racket)               ; a fully conniving family of lisps
        sh                                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web                              ; the tubes
+       ;;web                              ; the tubes
        ;;(yaml +lsp)                      ; JSON, but readable
        ;;zig                              ; C, but simpler
 
@@ -187,7 +187,7 @@
        ;;emms
        ;;everywhere                       ; *leave* EmaYou must be joking
        ;;irc                              ; how neckbeards socialize
-       rss                        ; emacs as an RSS reader, this should build with `libxml2'
+       ;;rss                        ; emacs as an RSS reader, this should build with `libxml2'
        ;;twitter           ; twitter client https://twitter.com/vnought;; ;;
 
        :config
